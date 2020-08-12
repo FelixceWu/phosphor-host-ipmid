378826c936c9fd06ea9aed1c8d3699f405091a47
fxn <you@example.com>
2020-08-13 02:58:08 +0800

    Test 3

---
+++ oemhandler.cpp
@@ -0,0 +1,31 @@
+#include <ipmid/api.hpp>
+#include <iostream>
+
+// Declares register function with attribute "constructor".
+void register_netfn_oem_functions() __attribute__((constructor));
+
+// Callback function for handling Oem command 0xBA.
+ipmi_ret_t ipmi_my_handler1(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                            ipmi_request_t request, ipmi_response_t response,
+                            ipmi_data_len_t data_len, ipmi_context_t context)
+{
+    std::cout << __FUNCTION__ << " entered! " << std::endl;
+
+    ipmi_ret_t rc = IPMI_CC_OK;
+    uint8_t rsp[] = {0xFF, 0x00, 0xAA, 0x55};
+    memcpy(response, &rsp, 4);
+    *data_len = 4;
+
+    return rc;
+}
+
+// Register function
+void register_netfn_oem_functions()
+{
+    std::cout << "Registering OEM command handlers" << std::endl;
+    ipmi_register_callback(NETFUN_OEM, 0xBA, NULL, ipmi_my_handler1, PRIVILEGE_USER);
+
+    return;
+}
+
+
